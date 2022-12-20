bac7fd619fb2d1338f0bc52c4e4a3a724e8b602a
canerariik <canerariik@hotmail.com>
2022-12-20 19:16:21 +0300

    pull-request-try

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
 print("This is a simple pull request project.")
+print("Şevval arkadaşımın repo içeriğini düzenlemiş olup, Pull-Request denemesini gerçekleştireceğim.")
+print("Önce Şevval'in hesabından ilgili repo fork edildi sonra clone edilip eklemeler dahil edildi ve sonrasında cmd üzerinde Pull-Request için ilgili kodlar yazılarak yapıldı.")
